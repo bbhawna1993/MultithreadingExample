7226598f5608f26b0ebef3e23ea0e9f0354fb8c8
bhawna <bhawnabudhiraja1993@gmail.com>
2019-04-28 16:50:20 +0530

    adding slide rotator

---
+++ src/DemoSleepMethod.java
@@ -0,0 +1,13 @@
+
+public class DemoSleepMethod {
+
+	public static void main(String[] args) throws InterruptedException{
+
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("slide-"+i);
+			Thread.sleep(5000);
+		}
+	}
+
+}
