9e9dd934db00d53587650379fd61ad5874b0fc34
bhawna <bhawnabudhiraja1993@gmail.com>
2019-04-28 22:05:06 +0530

    interupt demo

--- src/InterruptedDemoForTargetThread.java
+++ src/InterruptedDemoForTargetThread.java
@@ -2,13 +2,13 @@
 class DemoThread extends Thread{
 	@Override
 	public void run() {
-		for(int i=0;i<1000;i++)
+		for(int i=0;i<10000;i++)
 		{
-			System.out.println("inside waiting thread"+i);
+			System.out.println("I am a lazy thread"+i);
 		}
 		System.out.println("I want to sleep");
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(10000);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			System.out.println("I got interrupted");
